83f557ecb496b8ec3b96eead61cf31ee70a82158
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-05-02 15:12:03 -0300

    array de fechas
    porque me autoingresa fechas, validacion eta bien?

---
+++ Ejercicio41/Ejercicio41/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime[] fechasArray = new DateTime[5];
+            int contador = 0;
+
+            bool ingresoCorrecto;
+            bool ok = false;
+            string ingreso;
+
+
+
+            Console.WriteLine("Ingrese una serie de fechas:");
+
+
+            do
+            {
+                ingreso = Console.ReadLine();
+                ingresoCorrecto = DateTime.TryParse(ingreso, out DateTime fechaValida);
+                if (!ingresoCorrecto)
+                {
+                    Console.WriteLine("Fecha invalida, ingrese otra: ");
+                    continue; //continue vuelve al inicio del ciclo , si hay un error conitnua, do for while for each
+                }
+                else
+                {
+
+                    fechasArray[contador] = fechaValida;
+                    contador++;
+                    Console.WriteLine("Quiere ingresar otra Fecha? S/N");
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.N)
+                    {
+                        ok = true;
+                    }
+                   
+                }
+            } while (contador > fechasArray.Length || !ok);
+            Array.Sort(fechasArray);
+
+            for (int i = 0; i < fechasArray.Length; i++)
+            {
+                Console.WriteLine(fechasArray[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+               
+
+            
+             
+ 
+
